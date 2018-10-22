52de29c157e2038022792f5a4d2328de5a72ef6b
marcelogdeandrade <marcelogdeandrade@gmail.com>
2018-10-22 16:29:37 -0300

    finishes setup.py and adds requirements

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import my_hello.my_hello as my_hello
 
 my_hello.world()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='my_hello_marcelo',
+      version='0.1',
+      packages=['my_hello'],
+      install_requires=['numpy'],
+      scripts=['scripts/hello.py']
+     ) 
+
